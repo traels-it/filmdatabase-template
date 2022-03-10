e9ec920b24f7ffb7457337477d9b6a29e5294ca8
Claus Holmgaard <holmgaard@traels.it>
2022-03-10 15:20:30 +0100

    Refactor. Better way to handle movie object.
    [#181492950]

--- app/controllers/omdb_import_controller.rb
+++ app/controllers/omdb_import_controller.rb
@@ -1,17 +1,16 @@
 class OMDBImportController < ApplicationController
   def create
-    # print("PARAMS: #{params["movie"]}")
-
     movie_json = get_omdb_json(params["movie"]["imdb_id"])
 
-    movie = Movie.new
-    movie.title = movie_json["Title"]
-    movie.release_date = movie_json["Released"]
-    movie.genre = movie_json["Genre"]
-    movie.director = movie_json["Director"]
-    movie.actors = movie_json["Actors"]
-    movie.plot = movie_json["Plot"]
-    movie.image_url = movie_json["Poster"]
+    movie = Movie.new(
+      title: movie_json["Title"],
+      release_date: movie_json["Released"],
+      genre: movie_json["Genre"],
+      director: movie_json["Director"],
+      actors: movie_json["Actors"],
+      plot: movie_json["Plot"],
+      image_url: movie_json["Poster"]
+    )
 
     respond_to do |format|
       if movie.save
@@ -29,6 +28,6 @@ class OMDBImportController < ApplicationController
   BASE_URL = "http://www.omdbapi.com/"
 
   def get_omdb_json(imdb_id)
-    imdb_id.nil? ? nil : HTTParty.get("#{BASE_URL}?apikey=#{Rails.application.credentials.omdb_api_key}&i=#{imdb_id}")
+    HTTParty.get("#{BASE_URL}?apikey=#{Rails.application.credentials.omdb_api_key}&i=#{imdb_id}")
   end
 end
