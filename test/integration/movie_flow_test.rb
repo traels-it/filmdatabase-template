7deaf275901bfb6b794f1a90ccd7e5cae824fb81
Claus Holmgaard <holmgaard@traels.it>
2022-03-16 09:35:10 +0100

    Fixed VCR in system test

--- test/integration/movie_flow_test.rb
+++ test/integration/movie_flow_test.rb
@@ -1,10 +1,6 @@
 require "test_helper"
 
 class MovieFlowTest < ActionDispatch::IntegrationTest
-  setup do
-    @testMovie = movies(:one)
-  end
-
   test "can see movie list" do
     get "/"
     assert_select "h1", "Movies"
--- test/system/omdb_movies_test.rb
+++ test/system/omdb_movies_test.rb
@@ -1,6 +1,10 @@
 require "application_system_test_case"
 
 class OMDBMoviesTest < ApplicationSystemTestCase
+  setup do
+    VCR.insert_cassette("omdb_import_cassette", record: :new_episodes)
+  end
+
   test "can visit search" do
     visit omdb_movies_url
     assert_selector "h1", text: "Search for a movie to add"
@@ -12,5 +16,10 @@ class OMDBMoviesTest < ApplicationSystemTestCase
     fill_in "query", with: "123"
     click_on "Search"
     first(".card-header").click_on("Add")
+    assert_selector "p", text: "Movie was successfully created."
+  end
+
+  teardown do
+    VCR.eject_cassette
   end
 end
