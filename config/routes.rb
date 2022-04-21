3f287e4a246ec7bb3ffa3dcf434041c43ce7c723
Corfitz <corfitz@traels.it>
2022-04-21 09:17:08 +0200

    Merging

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "movies#index"
-  
-  resources :movies
+
+  resources :movies do
+    collection do
+      post :search
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
